fe18b6c5e443d9cc087987bf7f55a46434d70ca2
Agustin Macia del Rio <agustinmaciadelrio@MacBook-Pro-de-Agustin.local>
2020-05-10 17:24:20 -0300

    ListTile & Divider

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+  final opciones = ['Uno','Dos','Tres','Cuatro','Cinco','Seis'];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsCorto()
+      ),
     );
   }
+
+
+  List<Widget> _crearItems(){
+    
+    List<Widget> lista = new List<Widget>();
+
+    for (String opcion in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opcion),
+      );
+
+      lista..add(tempWidget)
+           ..add(Divider());
+    }
+    
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto() {
+
+    return opciones.map((widget){
+
+        return Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(widget + '!'),
+            ),
+            Divider(),
+          ],
+        );
+
+    }).toList();
+
+  }
+
+
 }
